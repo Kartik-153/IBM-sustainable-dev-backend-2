64655673bff2c052ef08c6f1ef8a0533160ef78b
Kartik-153 <106980372+Kartik-153@users.noreply.github.com>
2024-10-07 21:32:53 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,7 +1,7 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://udityaprakash983:8GwPYGhXNOh1xnkH@cluster0.iyvc2xz.mongodb.net/ibm"
+uri = "mongodb+srv://udityaprakash983:<PASSWORD>@cluster0.iyvc2xz.mongodb.net/ibm"
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
